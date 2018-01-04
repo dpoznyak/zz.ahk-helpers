eb2ea5b67bff1a2b3bf4a83a717fa174b02d421f
DPLV\dpoz <dpoznyak@gmail.com>
2018-01-05 01:04:34 +0800

    taskview works

---
+++ TaskView/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsInput.Native;// Importante
+
+namespace taskview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsInput.InputSimulator kb = new WindowsInput.InputSimulator();
+            kb.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.TAB);
+            //Console.Read();// Keep console window open
+        }
+    }
+}
